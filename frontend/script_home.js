e4f8b73191553519e38c514b0074a4b3b602d01f
PinkDolphin223 <andreea.niculae223@gmail.com>
2019-01-15 20:36:11 +0200

    whole UI done

---
+++ frontend/script_home.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function(){
+   
+   
+   
+    let http = new XMLHttpRequest();
+    http.open('get','http://188.25.199.62:3000/games',true);
+    http.setRequestHeader('Content-type', 'application/json');
+
+    http.send();
+
+    http.addEventListener('readystatechange',function(){
+
+        if(http.readyState==4){
+            var games = JSON.parse(http.response);
+            console.log(games);
+            var imgElement = document.querySelector("#game_image_4");
+            let game4 = games[4];
+            
+            http.open('get','http://188.25.199.62:3000/images/'+game4.cover.path+"",true);
+            http.send();
+            
+            http.addEventListener('readystatechange',()=>{
+                if(http.readyState==4){
+                    imgElement.src = http.response;
+                }
+            })
+
+    }});
+    
+
+
+});
